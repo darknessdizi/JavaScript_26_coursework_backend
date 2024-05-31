b13b974a25014994b3b218f8c186fdfa12bf5560
Remezov Dmitriy <darknessdizi@gmail.com>
2024-05-31 23:33:49 +0700

    Добавлена папка статики и логика сохраненя файлов

--- db/index.js
+++ db/index.js
@@ -1,24 +1,68 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid'); // импортируем v4 из uuid и переименовываем как uuidv4
+const fs = require('fs');
 
-const dataBase = {
-  data: [],
+class DataFiles {
+  constructor() {
+    this.data = [];
+  }
 
-  addData(obj) {
-    // Добавляем объект сообщения в общий список
-    if (obj.type === 'message') {
-      const result = {
-        id: uuidv4(),
-        timestamp: Date.now(),
-        ...obj,
+  init() {
+    // инициализация класса
+    fs.stat('./public/dataBase.json', (err) => {
+      if (err === null) {
+        console.log('File dataBase.json exists');
+      } else if (err.code === 'ENOENT') {
+        // file does not exist
+        fs.open('./public/dataBase.json', 'w', (error) => {
+          if (error) throw error;
+          console.log('File created');
+        }); 
+      } else {
+        console.log('Some other error: ', err.code);
       }
-      this.data.push(result);
-      return result;
+    });
+
+    fs.readFile('./public/dataBase.json', 'utf8', this.initFile.bind(this));
+  }
+
+  initFile(error, fileData) {
+    // Callback - чтение файла со списком сообщений
+    if (error) { // если возникла ошибка
+      return console.log(error);
     }
-  },
+    if (!fileData) { // если файл пустой, создать в нем структуру записей 
+      this.saveFile();
+    }
+    const json = JSON.parse(fileData);
+    // /* eslint-disable-next-line */
+    for (const obj of json.messages) {
+      this.data.push(obj);
+    }
+    return null;
+  }
 
-  getData() {
+  addData({ type, cords, content } = {}, file = null) {
+    // Добавляем объект сообщения в общий список
+    let result = { type, cords, content };
+    result.id = uuidv4();
+    result.timestamp = Date.now();
+    this.data.push(result);
+    this.saveFile(this.data); 
+    return result;
+  }
+
+  saveFile(data = []) {
+    // Сохраняет изменения в файл
+    const body = { messages: data };
+    const file = JSON.stringify(body, null, 2);
+    fs.writeFileSync('./public/dataBase.json', file);
+  }
+
+  getData() { 
     return this.data;
-  },
-};
+  }
+}
 
-module.exports = dataBase;
+module.exports = {
+  DataFiles, 
+};
--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const cors = require('@koa/cors'); // правила политики cors
+const koaStatic = require('koa-static');
+const path = require('path');
 // const router = require('./routes'); // импортируем набор роутеров по файлу index.js в папке
 const Router = require('koa-router');
 const http = require('http');
 const WS = require('ws'); // сервер для WebSocket от клиентов
 // const delay = require('koa-delay'); // задержка ответа сервера delay(2, 3) 2 - до, 3 - после ответа
 // const slow = require('koa-slow'); // задержка ответа сервера
-const dataBase = require('./db');
+// const dataBase = require('./db');
 // const websockify = require('koa-websocket');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid'); // импортируем v4 из uuid и переименовываем как uuidv4
+const { DataFiles } = require('./db');
+
+function sendAllUsers(message, sender) {
+	Array.from(wsServer.clients)
+		.filter((client) => client.readyState === WS.OPEN)
+		.forEach((client) => client.send(message));
+}
+
+const dataBase = new DataFiles();
+dataBase.init();
 
 const app = new Koa();
+
+const public = path.join(__dirname, '/public');
+app.use(koaStatic(public)); // Дает возможность раздавать файлы
 
 app.use(koaBody({ // чтобы обработать тело запроса
   // (обязательно объявить до Middleware где работаем с body)
@@ -45,8 +62,10 @@ const wsServer = new WS.Server({
   server
 });
 
-router.post('/', (ctx) => {
+router.post('/message', (ctx) => {
   console.log('POST запрос на сервер от:', ctx.request.header.referer); // показать url источника запроса
+  console.log('POST тело:', ctx.request.body);
+  // console.dir(ctx.request.files);
   if (ctx.request.body.type === 'message') {
     const obj = {
       status: 'addMessage',
@@ -54,6 +73,36 @@ router.post('/', (ctx) => {
     };
     ctx.response.status = 200;
     sendAllUsers(JSON.stringify(obj));
+  }
+});
+
+router.post('/unload', async (ctx) => {
+  const { body } = ctx.request;
+  console.log('************* upload *************', body);
+  // console.dir(ctx.request.files);
+  const file = ctx.request.files.file; // Get upload files
+  console.log('file', file.name, file.type);
+  const reader = fs.createReadStream(file.path); // Create-readable stream
+  const ext = file.name.split('.').pop(); // Get upload the file extension
+  console.log('ext', ext);
+  let upStream = null;
+  if (file.type === 'video/webm') {
+    const name = `record.${uuidv4()}.${ext}`;
+    upStream = fs.createWriteStream(`public/${name}`);
+    body.content = {};
+    body.content.name = name;
+    body.content.originalName = file.name;
+    body.content.path = `/${name}`;
+    reader.pipe(upStream);
+  }
+  if (body.type === 'video') {
+    const obj = { 
+      status: 'addVideo',
+      result: dataBase.addData(body),
+    };
+    ctx.response.status = 200;
+    setTimeout(() => sendAllUsers(JSON.stringify(obj)), 100);
+    // поставил таймер с целью избежать Error: write ECONNABORTED
   }
 });
 
@@ -67,41 +116,7 @@ wsServer.on('connection', (ws) => { // ws и есть сам клиент
   ws.on('close', (number) => {
     console.log('Соединение закрыто', number);
   });
-}); 
-
-
-//   // ws.on('message', (body) => {
-//     // ws.send(JSON.stringify({ status: 'yes' }))
-//     // const obj = JSON.parse(body);
-//     // let id = null;
-//     // if (obj.command === 'Create command') {
-//     //   id = uuid.v4();
-//     //   createInstance(ws, id);
-//     // }
-//     // if (obj.command === 'Delete command') {
-//     //   deleteInstance(ws, obj);
-//     // }
-//     // if ((obj.command === 'Start command') || (obj.command === 'Pause command')) {
-//     //   changeInstance(ws, obj);
-//     // }
-//     // if (id === null) {
-//     //   id = obj.id;
-//     // }
-//     // ws.send(JSON.stringify({ 
-//     //   status: `Received`, 
-//     //   data: {
-//     //     id,
-//     //     command: obj.command,
-//     //     time: Date.now(),
-//     //   }
-//     // }));
-//   // });
-
-function sendAllUsers(message, sender) {
-	Array.from(wsServer.clients)
-		.filter((client) => client.readyState === WS.OPEN)
-		.forEach((client) => client.send(message));
-}
+});
 
 server.listen(port, (err) => {
   // два аргумента (1-й это порт, 2-й это callback по результатам запуска сервера)
